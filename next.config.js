759e523383c294eb3462e1defeb44970a6006455
ruyyi0323 <ruyyi0323@gmail.com>
2025-02-08 20:04:27 -0500

    update the next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+    ];
+  },
   output: "standalone"
-}
+};
+
+module.exports = nextConfig;
