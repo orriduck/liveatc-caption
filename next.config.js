12b6c4c7dde764f66306ebcc72f4fb5812d98ab1
ruyyi0323 <ruyyi0323@gmail.com>
2025-02-08 20:28:50 -0500

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         source: "/api/:path*",
         destination:
           process.env.VERCEL_ENV
-            ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/:path*`
+            ? `/api/`
             : `http://${process.env.NEXT_PUBLIC_LOCAL_ENDPOINT_URL}/api/:path*`,
       },
     ];
