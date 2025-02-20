9664d1e8dd1e05420206dcd75d4d6a8aaf607549
ruyyi0323 <ruyyi0323@gmail.com>
2025-02-20 01:21:55 -0500

    clean fallback

--- app/(pages)/player/page.tsx
+++ app/(pages)/player/page.tsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 
-import AudioChannelPlayer from '@/components/player/audio-channel-player';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, Suspense } from 'react';
-import { useRouter } from 'next/navigation';
-import { AudioChannel } from '@/types/airport';
-import PageHeader from '@/components/page-header';
+import AudioChannelPlayer from "@/components/player/audio-channel-player";
+import { useSearchParams } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import { useRouter } from "next/navigation";
+import { AudioChannel } from "@/types/airport";
+import PageHeader from "@/components/page-header";
 
 function PlayerContent() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const channelParam = searchParams.get('channel');
+  const channelParam = searchParams.get("channel");
 
   let channel: AudioChannel | null = null;
   try {
@@ -18,12 +18,12 @@ function PlayerContent() {
       channel = JSON.parse(decodeURIComponent(channelParam));
     }
   } catch (error) {
-    console.error('Error parsing channel data:', error);
+    console.error("Error parsing channel data:", error);
   }
 
   useEffect(() => {
     if (!channel) {
-      router.push('/');
+      router.push("/");
     }
   }, [channel, router]);
 
@@ -32,15 +32,23 @@ function PlayerContent() {
   return (
     <div className="flex flex-col space-y-8">
       <PageHeader />
-
       <AudioChannelPlayer audioChannel={channel} />
+    </div>
+  );
+}
+
+function FallBackContent() {
+  return (
+    <div className="flex flex-col space-y-8">
+      <PageHeader />
+      <div className="animate-pulse">Loading...</div>
     </div>
   );
 }
 
 export default function PlayerPage() {
   return (
-    <Suspense fallback={<div className="flex flex-col space-y-8"><PageHeader /><div className="animate-pulse">Loading...</div></div>}>
+    <Suspense fallback={<FallBackContent />}>
       <PlayerContent />
     </Suspense>
   );
