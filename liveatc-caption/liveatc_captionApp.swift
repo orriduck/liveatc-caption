f30bd13cf072056dfd936be43166f104d984266e
ruyyi0323 <ruyyi0323@gmail.com>
2025-11-19 09:07:26 -0500

    Initial Commit

---
+++ liveatc-caption/liveatc_captionApp.swift
@@ -0,0 +1,17 @@
+//
+//  liveatc_captionApp.swift
+//  liveatc-caption
+//
+//  Created by Chen Liang on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct liveatc_captionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
