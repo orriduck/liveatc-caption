78d9fe4d89dbba99ecb146f35b6ae131c0decafc
ruyyi0323 <ruyyi0323@gmail.com>
2026-04-27 08:35:32 -0400

    try import vercel monitoring

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="min-h-screen bg-atc-bg text-atc-text">
-    <router-view v-slot="{ Component }">
-      <transition name="screen" mode="out-in">
-        <component :is="Component" />
-      </transition>
-    </router-view>
-  </div>
+    <div class="min-h-screen bg-atc-bg text-atc-text">
+        <router-view v-slot="{ Component }">
+            <transition name="screen" mode="out-in">
+                <component :is="Component" />
+            </transition>
+        </router-view>
+    </div>
 </template>
 
 <script setup>
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 </script>
